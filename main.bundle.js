629cb8753ce413e2902f2b737597a5b168333b6c
Jesse Smith <jessesrsmith2@gmail.com>
2017-07-07 15:12:47 -0600

    Update jsgame description

--- main.bundle.js
+++ main.bundle.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
@@ -45,14 +44,22 @@
 /* 0 */
 /***/ function(module, exports, __webpack_require__) {
 
+	__webpack_require__(1);
+	(function webpackMissingModule() { throw new Error("Cannot find module \"build\""); }());
+
+
+/***/ },
+/* 1 */
+/***/ function(module, exports, __webpack_require__) {
+
 	'use strict';
 
-	var Blanks = __webpack_require__(1);
-	var Letterboard = __webpack_require__(2);
-	var Man = __webpack_require__(3);
-	var Score = __webpack_require__(4);
-	var Check = __webpack_require__(5);
-	var getWord = __webpack_require__(7);
+	var Blanks = __webpack_require__(2);
+	var Letterboard = __webpack_require__(3);
+	var Man = __webpack_require__(4);
+	var Score = __webpack_require__(5);
+	var Check = __webpack_require__(6);
+	var getWord = __webpack_require__(8);
 
 	jQuery(function ($) {
 
@@ -125,7 +132,7 @@
 	});
 
 /***/ },
-/* 1 */
+/* 2 */
 /***/ function(module, exports) {
 
 	"use strict";
@@ -163,7 +170,7 @@
 	module.exports = Blanks;
 
 /***/ },
-/* 2 */
+/* 3 */
 /***/ function(module, exports) {
 
 	"use strict";
@@ -180,7 +187,7 @@
 	module.exports = Letterboard;
 
 /***/ },
-/* 3 */
+/* 4 */
 /***/ function(module, exports) {
 
 	'use strict';
@@ -249,7 +256,7 @@
 	module.exports = Man;
 
 /***/ },
-/* 4 */
+/* 5 */
 /***/ function(module, exports) {
 
 	'use strict';
@@ -280,12 +287,12 @@
 	module.exports = Score;
 
 /***/ },
-/* 5 */
+/* 6 */
 /***/ function(module, exports, __webpack_require__) {
 
 	"use strict";
 
-	var invalidAlert = __webpack_require__(6);
+	var invalidAlert = __webpack_require__(7);
 
 	var Check = {
 	  valid: function valid(guess, guessed) {
@@ -307,7 +314,7 @@
 	module.exports = Check;
 
 /***/ },
-/* 6 */
+/* 7 */
 /***/ function(module, exports) {
 
 	'use strict';
@@ -322,7 +329,7 @@
 	module.exports = invalidAlert;
 
 /***/ },
-/* 7 */
+/* 8 */
 /***/ function(module, exports) {
 
 	"use strict";
@@ -361,6 +368,3 @@
 
 /***/ }
 /******/ ]);
-=======
-!function(t){function n(s){if(e[s])return e[s].exports;var o=e[s]={exports:{},id:s,loaded:!1};return t[s].call(o.exports,o,o.exports,n),o.loaded=!0,o.exports}var e={};return n.m=t,n.c=e,n.p="",n(0)}([function(t,n,e){"use strict";var s=e(6),o=e(21),i=e(22),r=e(7),h=e(4),c=e(8);jQuery(function(t){var n={init:function(){this.score=new r,this.bindEvents(),this.start()},bindEvents:function(){t("#guess-form").on("submit",this.submit.bind(this)),t(document).on("click","#new-round-button",this.start.bind(this)).on("hide.bs.modal","#endModal",this.start.bind(this))},start:function(){var t=this;c("http://www.setgetgo.com/randomword/get.php").then(function(n){t.answer=n,t.blanks=new s(n)},function(t){console.error("Failed!",t)}),this.guessed=[],this.incorrect=0,this.man=new i,this.letterboard=new o},submit:function(n){n.preventDefault();var e=t("#guess");this.guess=e.val().toLowerCase(),e.val(""),e.focus(),h.valid(this.guess,this.guessed)&&(this.guessed.push(this.guess),this.letterboard.print(this.guess),h.correct(this.guess,this.answer)?this.right():this.wrong())},right:function(){this.blanks.replace(this.guess),this.blanks.currentBlanks===this.answer&&(this.score.printRoundsWon(),t("#endModalLabel").html("You Win!"),t("#endModal").modal("show"))},wrong:function(){this.man.draw(this.incorrect++),7===this.incorrect&&(this.blanks.reveal(),this.score.printRoundsLost(),t("#endModalLabel").html("You Loose!"),t("#endModal").modal("show"))}};n.init()})},,,,function(t,n,e){"use strict";var s=e(5),o={valid:function(t,n){return/[a-z]/.test(t)?!(n.indexOf(t)>=0)||(s("Already guessed!"),!1):(s("Not a letter!"),!1)},correct:function(t,n){return n.indexOf(t)>=0}};t.exports=o},function(t,n){"use strict";function e(t){var n=$("#myAlert");n.text(t),n.fadeTo(400,1),n.fadeTo(800,0)}t.exports=e},function(t,n){"use strict";function e(t){this.answer=t,this.currentBlanks=this.getBlanks(),this.render()}e.prototype.getBlanks=function(){return this.answer.replace(/[a-zA-Z-]/g,"_")},e.prototype.render=function(){var t=$("#blanks");t.empty(),t.append(this.currentBlanks)},e.prototype.replace=function(t){for(var n=this.answer.indexOf(t);n>=0;)this.currentBlanks=this.currentBlanks.substr(0,n)+t+this.currentBlanks.substr(n+1),n=this.answer.indexOf(t,n+1);this.render()},e.prototype.reveal=function(){this.currentBlanks=this.answer,this.render()},t.exports=e},function(t,n){"use strict";function e(){this.roundsWon=0,this.roundsLost=0,this.$won=$("#won"),this.$lost=$("#lost"),this.printRounds()}e.prototype.printRounds=function(){this.$won.append(this.roundsWon),this.$lost.append(this.roundsLost)},e.prototype.printRoundsWon=function(){this.roundsWon++,this.$won.text(this.roundsWon)},e.prototype.printRoundsLost=function(){this.roundsLost++,this.$lost.text(this.roundsLost)},t.exports=e},function(t,n){"use strict";function e(t){return new Promise(function(n,e){var s=new XMLHttpRequest;s.open("GET",t),s.onload=function(){200===s.status?n(s.response):e(Error(s.statusText))},s.onerror=function(){e(Error("Network Error"))},s.send()})}t.exports=e},,,,,,,,,,,,,function(t,n){"use strict";function e(){this.$board=$("#letterboard"),this.$board.empty()}e.prototype.print=function(t){this.$board.prepend(t+" ")},t.exports=e},function(t,n){"use strict";function e(){this.drawOrder=[this.drawHead,this.drawTorso,this.drawLeftArm,this.drawRightArm,this.drawLeftLeg,this.drawRightLeg,this.drawNoose],this.canvas=document.getElementById("canvas"),this.context=this.canvas.getContext("2d"),this.centerX=this.canvas.width/2,this.context.strokeStyle="#003300",this.context.lineWidth=3,this.context.clearRect(0,0,this.canvas.width,this.canvas.height)}e.prototype.draw=function(t){this.drawOrder[t].apply(this),this.context.stroke()},e.prototype.drawHead=function(){this.context.beginPath(),this.context.arc(this.centerX,100,25,0,2*Math.PI,!1)},e.prototype.drawTorso=function(){this.context.beginPath(),this.context.moveTo(this.centerX,125),this.context.lineTo(this.centerX,200)},e.prototype.drawLeftArm=function(){this.context.beginPath(),this.context.moveTo(this.centerX,135),this.context.lineTo(140,150)},e.prototype.drawRightArm=function(){this.context.beginPath(),this.context.moveTo(this.centerX,135),this.context.lineTo(210,150)},e.prototype.drawLeftLeg=function(){this.context.beginPath(),this.context.moveTo(this.centerX,200),this.context.lineTo(140,250)},e.prototype.drawRightLeg=function(){this.context.beginPath(),this.context.moveTo(this.centerX,200),this.context.lineTo(210,250)},e.prototype.drawNoose=function(){this.context.beginPath(),this.context.moveTo(this.centerX,75),this.context.lineTo(this.centerX,25),this.context.lineTo(250,25),this.context.lineTo(250,275),this.context.moveTo(300,275),this.context.lineTo(200,275)},t.exports=e}]);
->>>>>>> 08b9814962273bae2929a254b1621c1f152ebdc3
